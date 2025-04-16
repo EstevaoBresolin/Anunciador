1ffc392db71380c5e37768bccd2bed6812567ea8
Estevão <estevaobresolin@hotmail.com>
2025-04-16 10:18:42 -0300

    ajustes vercel

---
+++ vercel-build.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+echo "⚙️ Iniciando build Blazor WASM..."
+
+dotnet workload install wasm-tools
+dotnet restore
+dotnet publish -c Release -o output
+
+# Copia o wwwroot para a pasta raiz do deploy
+cp -r output/wwwroot/ wwwroot
